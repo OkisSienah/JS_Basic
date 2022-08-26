2cd2b36ea30fb80c5f1cea410a6c4d0668bd6efc
OkisSienah <104500247+OkisSienah@users.noreply.github.com>
2022-08-26 14:46:27 +0700

    Delete vp.js

--- vp.js
+++
@@ -1,7 +0,0 @@
-let errPar
-
-function purchase() {
-    errPar = document.getElementById("error")
-    console.log("btn was clicked")
-    errPar.textContent = "Something went wrong, Please try again later!"
-}
